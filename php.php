c002a80bd790b9247d60992352514f1433d677d1
AravindhakumarG <117360996+aravindhakumar-dev@users.noreply.github.com>
2023-01-21 18:49:50 +0530

    5.1

---
+++ php.php
@@ -0,0 +1,8 @@
+        echo '<div class="slider-items">
+            <a href="' . get_term_link( $product_category ) . '">
+                <h2>
+                    <img src="'.$thumbnail_url.'" />
+                    ' . $product_category->name . '
+                </h2>
+            </a>
+        </div>';
